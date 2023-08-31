c65bb5e99f89865d3db9a9eae503914d03f1d83e
Pedro Henrique Alves <118781462+pedrohenriquealvesfernandes@users.noreply.github.com>
2023-08-31 15:29:29 -0300

    finalizando porfolio

---
+++ js/darkmode.js
@@ -0,0 +1,9 @@
+/* Theme */
+
+const changeTheme = document.getElementById("change-theme");
+const themeCheck = document.querySelector(".box-check");
+
+themeCheck.addEventListener("click", () => {
+  changeTheme.checked = !changeTheme.checked;
+  document.body.classList.toggle("dark", !changeTheme.checked);
+});
--- js/script.js
+++ js/script.js
@@ -1,81 +1,57 @@
 /* Variaveis */
 
-/* Theme */
-
-const changeTheme = document.getElementById("change-theme")
-const themeCheck = document.querySelector(".box-check")
-
 /* Menu e navbar-lista */
-const menuHamburguer = document.getElementById("hamburguer-menu-wrapper")
-const navLista = document.querySelector(".navbar-lista")
-const hamburguerInp = document.getElementById("hamburguerInp")
+const menuHamburguer = document.getElementById("hamburguer-menu-wrapper");
+const navLista = document.querySelector(".navbar-lista");
+const hamburguerInp = document.getElementById("hamburguerInp");
 
-const sections = document.querySelectorAll('section')
-const navLinks = document.querySelectorAll('.nav_link')
-const cabecalho = document.querySelector(".cabecalho")
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav_link");
+const cabecalho = document.querySelector(".cabecalho");
 
 /* Efeito hidden */
-const hiddenElements = document.querySelectorAll('.hidden')
+const hiddenElements = document.querySelectorAll(".hidden");
 
-
-/* --------------------------------------------------------- */
-
-/* Theme */
-themeCheck.addEventListener('click',()=>{
-    changeTheme.checked = !changeTheme.checked
-})
+/* -------------------------------------------------------- */
 
 /* Menu e navbar */
 
-menuHamburguer.addEventListener('click',()=>{
-    hamburguerInp.checked = !hamburguerInp.checked
+menuHamburguer.addEventListener("click", () => {
+  hamburguerInp.checked = !hamburguerInp.checked;
 
-    if (navLista.style.display === 'flex') {
-        navLista.style.display = 'none';
-        document.querySelector(".cabecalho").style.borderBottom = '1px solid black'; 
-    } else {
-        navLista.style.display = 'flex';
-        document.querySelector(".cabecalho").style.borderBottom = 'none'; 
+  navLista.classList.toggle("show-nav");
+});
+
+let currentSection = "home";
+window.addEventListener("scroll", () => {
+  sections.forEach((sec) => {
+    if (window.scrollY >= sec.offsetTop + 200) {
+      currentSection = sec.id;
     }
-})
+  });
 
-
-let currentSection = 'home'
-window.addEventListener('scroll',()=>{
-    sections.forEach(sec =>{
-        if(window.scrollY >= (sec.offsetTop + 200)){
-            currentSection = sec.id;
-        }
-    })
-
-    navLinks.forEach(navLink =>{
-        if(navLink.href.includes(currentSection)){
-            navLink.classList.add('active')
-        }else {
-            navLink.classList.remove('active');
-        }
-    })
-})
+  navLinks.forEach((navLink) => {
+    if (navLink.href.includes(currentSection)) {
+      navLink.classList.add("active");
+    } else {
+      navLink.classList.remove("active");
+    }
+  });
+});
 
 /* Efeito hidden com observer */
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry)=> {
-        console.log(entry)
-        if (entry.isIntersecting){
-            entry.target.classList.add('show')
-        }
-    })
-})
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
 
 hiddenElements.forEach((el) => observer.observe(el));
 
 /* Copyright data */
-const anoAtual = new Date().getFullYear()
-const copyright = document.querySelector(".copyright")
-copyright.innerHTML = `&copy;${anoAtual} Pedro Alves. Todos os direitos reservados.`
-
-
-
-
-
-
+const anoAtual = new Date().getFullYear();
+const copyright = document.querySelector(".copyright");
+copyright.innerHTML = `&copy;${anoAtual} Pedro Alves.`;
